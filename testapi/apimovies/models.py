79d0ff9c4bc91e7937656ff97de3b0913e77f58a
auscode <hm6091999@gmail.com>
2023-09-16 20:06:51 +0530

    extra field commited

--- testapi/apimovies/models.py
+++ testapi/apimovies/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)  # Change 'name' to 'title' for clarity
+    description = models.TextField()  # Allowing for longer descriptions
+    # release_date = models.DateField()
+    # genre = models.CharField(max_length=100)
+    # director = models.CharField(max_length=100)
+    # # actors = models.ManyToManyField("Actors" , related_name='movies')
+    # duration_minutes = models.PositiveIntegerField()
+    # poster_url = models.URLField()
+    # trailer_url = models.URLField()
     
     def __str__(self) -> str:
         return self.name
