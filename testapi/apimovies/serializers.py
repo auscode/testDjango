d4cd861ef0ff800ec85dedf31054fe5280ca0d30
auscode <hm6091999@gmail.com>
2023-09-16 19:59:46 +0530

    repaired

--- testapi/apimovies/serializers.py
+++ testapi/apimovies/serializers.py
@@ -4,4 +4,4 @@ from .models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id','name','description']
+        fields = '__all__'
